41150436fed52b43f089881aaf662724632231a9
Stevenleez <stevenaxel2003@gmail.com>
2025-09-21 15:48:53 +0700

    Commit No. 3 – Tabbed Navigation

---
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.lab_week_04
+
+import android.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+val TABS_FIXED = listOf(
+    R.string.starbucks_title,
+    R.string.janjijiwa_title,
+    R.string.kopikenangan_title,
+)
+class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return TABS_FIXED.size
+    }
+    override fun createFragment(position: Int): androidx.fragment.app.Fragment
+    {
+        return CafeDetailFragment()
+    }
+}
